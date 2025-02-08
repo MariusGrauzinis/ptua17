b164aec6ad84457023c13ceda3dd6c7d54f8953c
Mariusgrauzinis <mariusgrauzinis@yahoo.co.uk>
2025-02-08 15:36:27 +0200

    new

---
+++ pamoka10/calculator.py
@@ -0,0 +1,20 @@
+print(f"I AM IN CALCULATOR>PY __name__ is{__name__}")
+
+def add(x: int, y: int) -> int:
+    return x + y
+
+
+def sub(x: int, y: int) -> int:
+    return x - y
+
+
+def prod(x: int, y: int) -> int:
+    return x * y
+
+
+def div(x: int, y: int) -> int:
+    return x // y
+
+if __name__ == "__main__":
+    addition = add(5, 5)
+    print(addition)
--- pamoka10/main.py
+++ pamoka10/main.py
@@ -1,1 +1,23 @@
-wsaa
+# from calculator import add, sub, prod
+
+
+# print(add(2, 2))
+# print(sub(2, 2))
+# print(prod(2, 2))
+
+# import calculator as calc
+
+# print(calc.add(2, 2))
+# print(calc.sub(2, 2))
+
+# Importuoja Viska "Bloga praktika!!!!!"
+# from calculator import *  
+
+
+# print(add(2, 2))
+# print(sub(2, 2))
+# print(prod(2, 2))
+
+from calculator import add
+
+print(f"I am addind 2 and 2 result is: {add(2, 2)}")
